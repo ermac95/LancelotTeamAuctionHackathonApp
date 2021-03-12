921c15bb98f281f949385a3a20359ab97a98df02
Ermac95 <ermac95@mail.ru>
2021-03-12 22:58:49 +0300

    LoginRegisterRepository created

---
+++ app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/domain/repository/LoginRegisterRepository.kt
@@ -0,0 +1,43 @@
+package com.mycodeflow.lancelotteamauctionhackathonapp.domain.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class LoginRegisterRepository {
+
+    private val firebaseAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val userMutableLiveData = MutableLiveData<FirebaseUser>()
+
+    suspend fun login(email: String, password: String) = withContext(Dispatchers.IO){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Log.d("myLogs", "Failed to login, try again")
+                }
+            }
+    }
+
+    suspend fun register(email: String, password: String) = withContext(Dispatchers.IO) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                    //val currentUserDb = dataBaseReference?.child(currentUser.uid)
+                    // currentUserDb?.child("user_name")?.setValue(userName.text.toString())
+                    // currentUserDb?.child("user_email")?.setValue(userEmail.text.toString())
+                    //openFragment(FragsNav.LS)
+                } else {
+                    Log.d("myLogs", "Failed to login, try again")
+                }
+            }
+    }
+}
--- app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/presentation/viewmodels/SampleViewModelFactory.kt
+++ app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/presentation/viewmodels/SampleViewModelFactory.kt
@@ -2,13 +2,13 @@ package com.mycodeflow.lancelotteamauctionhackathonapp.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.mycodeflow.lancelotteamauctionhackathonapp.domain.repository.SampleRepository
+import com.mycodeflow.lancelotteamauctionhackathonapp.domain.repository.LoginRegisterRepository
 import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
 @Suppress("UNCHECKED_CAST")
 class SampleViewModelFactory @Inject constructor(
-    private val advListRepository: SampleRepository
+    private val advListRepository: LoginRegisterRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when(modelClass){
         SampleViewModel::class.java -> SampleViewModel(advListRepository)
