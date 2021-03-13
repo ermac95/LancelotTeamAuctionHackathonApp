b2dd78c41ee563f0a7e2faa544a711ba30568ce1
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2021-03-13 18:22:43 +0300

    Ресайклер + адаптер

--- app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/data/models/Advertisement.kt
+++ app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/data/models/Advertisement.kt
@@ -3,15 +3,17 @@ package com.mycodeflow.lancelotteamauctionhackathonapp.data.models
 import com.google.firebase.firestore.auth.User
 
 data class Advertisement(
-    val id: String,
-    val ownerUid: String,
-    val title: String?,
-    val poster: String?,
-    val images: List<ItemImage>?,
-    val price: Float?,
-    val betStep: Float?,
-    val description: String?,
-    val date: String?,
-    val time: String?,
+    val id: String = "",
+    val ownerUid: String = "",
+    val title: String? = "",
+    val poster: String? = "",
+    val images: List<ItemImage>? = emptyList(),
+    val price: Float? = 0f,
+    val betStep: Float? = 0f,
+    val description: String? = "",
+    val date: String? = "",
+    val time: String? = "",
     val participators: List<User>? = emptyList()
-)
+) {
+
+}
