52a97a793142b8769951d4fccff7b87b6ee8c82b
Ermac95 <ermac95@mail.ru>
2021-03-13 19:44:03 +0300

    AdvDetailRepository.kt created method for getting advertisement by id

---
+++ app/src/main/java/com/mycodeflow/lancelotteamauctionhackathonapp/domain/repository/AdvDetailRepository.kt
@@ -0,0 +1,26 @@
+package com.mycodeflow.lancelotteamauctionhackathonapp.domain.repository
+
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import com.mycodeflow.lancelotteamauctionhackathonapp.data.models.Advertisement
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class AdvDetailRepository @Inject constructor(
+    private val fireStore: FirebaseFirestore
+) {
+
+    suspend fun getAdvertisementById(advId: String): Advertisement? {
+        val data = loadAdvFromFireStore(advId)
+        val advertisement = data.toObject<Advertisement>()
+        advertisement
+    }
+
+    private suspend fun loadAdvFromFireStore(advId: String): DocumentSnapshot {
+        return fireStore.collection("advertisements")
+            .document(advId)
+            .get()
+            .await()
+    }
+}
